d02fc2d28fc052806ddc6d3344bb109d0569a81c
Pablo Vinícius <pablovgramos@gmail.com>
2024-01-12 10:05:20 -0300

    Leitura do json corrigida

--- item/script.js
+++ item/script.js
@@ -1,13 +1,18 @@
-const nome = document.getElementById("nome")
-const preco = document.getElementById("preco")
-const loja = document.getElementById("loja")
-const imagem = document.getElementById("imagem")
+var nome = document.getElementById("nome")
+var preco = document.getElementById("valor")
+var loja = document.getElementById("loja")
+var imagem = document.getElementById("espacoimagem")
 
 
 fetch('lista.json').then((response) => {
     response.json().then((dados) => {
-        const nomel = dados.nome
-        
+        var lista = dados
+        var img = lista['imagem']
+
+        nome.innerHTML = lista['nome']
+        preco.innerHTML = lista['preco']
+        loja.innerHTML = lista['loja']
+        imagem.style.backgroundImage = img
     })
 })
-nome.innerHTML = nomel
+
